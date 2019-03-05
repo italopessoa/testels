9577490e7439b22d34078269d43e7bbf5a8616db
Italo Pessoa <italo.pessoa@hotmail.com>
2019-03-05 10:30:41 -0300

    teste

---
+++ GenerateRandomNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MagicPackageTest
+{
+    public static class RandomGenerator
+    {
+        public static int Number => 3;
+
+        public static string StringG()
+        {
+            Console.WriteLine("gluglu ieie");
+            return "Random String";
+        }
+    }
+}
